10206683cf6a355ae524d87e63e07fcb36eb5177
yamako <yamako.windish@gmail.com>
2019-01-08 16:53:19 +0900

    2.8 算出プロパティ(computed)

--- chapter_2/app.js
+++ chapter_2/app.js
@@ -28,6 +28,18 @@ var vm = new Vue({
 			}
 			return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
 		}
+	},
+	computed: {
+		totalPrice: function() {
+			// this経由でインスタンス内のデータにアクセス
+			return this.items.reduce(function (sum, item) {
+				return sum + (item.price * item.quantity)
+			}, 0)
+		},
+		totalPriceWithTax: function() {
+			// 算出プロパティに依存した算出プロパティも定義できる
+			return Math.floor(this.totalPrice * 1.08)
+		}
 	}
 })
 
